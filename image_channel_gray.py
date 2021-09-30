ebbea3e8e2dab7f86f247640d482585d668d808a
sunabove <sunabove@nate.com>
2021-09-30 19:48:16 +0900

    **

---
+++ image_channel_gray.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+import cv2
+# Load a color image 
+img = cv2.imread('messi5.jpg')
+# print image size
+blue  = img[:, :, 0]  # blue channel data
+green = img[:, :, 1]  # green channel data
+red   = img[:, :, 2]  # red channel data
+
+fusion =  (blue + green + red)/3
+cv2.imshow('Fusion channels', fusion) 
+
+k = cv2.waitKey(0)
+if k == 27: cv2.destroyAllWindows() 
