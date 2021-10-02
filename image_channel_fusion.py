0dbbf50f3032294a7b797a3f9a7c3795de2e11d7
sunabove <sunabove@nate.com>
2021-10-02 20:53:15 +0900

    **

--- image_channel_fusion.py
+++ image_channel_fusion.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-import cv2
+import cv2, numpy as np
 # Load a color image 
 img = cv2.imread('messi5.jpg')
 
@@ -8,7 +8,8 @@ blue  = img[:, :, 0]  # blue channel data
 green = img[:, :, 1]  # green channel data
 red   = img[:, :, 2]  # red channel data
 
-fusion =  (blue + green + red)/3
+fusion =  (blue + green + red )/3
+
 cv2.imshow('Fusion channels', fusion) 
 
 cv2.waitKey(0)
--- image_channel_gray.py
+++ image_channel_gray.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-import cv2
+import cv2, numpy as np
 # Load a color image 
 img = cv2.imread('messi5.jpg')
 # split the image into 3 channel data
@@ -8,6 +8,9 @@ green = img[:, :, 1]  # green channel data
 red   = img[:, :, 2]  # red channel data
 
 fusion =  (blue + green + red)/3
+
+fusion = fusion.astype(np.uint8)
+
 cv2.imshow('Fusion channels', fusion) 
 
 k = cv2.waitKey(0)
