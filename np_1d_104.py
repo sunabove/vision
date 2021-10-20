55a259badec898707bba859c3cc92b3479e1370d
sunabove <sunabove@nate.com>
2021-10-21 00:24:12 +0900

    **

---
+++ np_1d_104.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+import numpy as np
+
+a = np.random.randint( 0, 10, 3 )
+b = np.random.rand( 3 )
+c = np.random.randn( 3 )
+d = np.random.uniform( 1, 10 ,3)
+e = np.random.normal( 5, 2, 3 )
+
+print( a )
+print( b )
+print( c )
+print( d )
+print( e )
